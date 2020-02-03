22c969c7542d7a1ab26b46e9e7822cae53aeda59
wedwardbeck <wedwardbeck@gmail.com>
2020-02-03 14:12:48 -0600

    Add Sentry - Poduction REQ & PY

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,11 @@
+import logging
+
+import sentry_sdk
+
+from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.logging import LoggingIntegration
+from sentry_sdk.integrations.celery import CeleryIntegration
+
 from .base import *  # noqa
 from .base import env
 
@@ -166,7 +174,7 @@ LOGGING = {
     "formatters": {
         "verbose": {
             "format": "%(levelname)s %(asctime)s %(module)s "
-            "%(process)d %(thread)d %(message)s"
+                      "%(process)d %(thread)d %(message)s"
         }
     },
     "handlers": {
@@ -195,6 +203,22 @@ LOGGING = {
         },
     },
 }
+
+# Sentry
+# ------------------------------------------------------------------------------
+SENTRY_DSN = env("SENTRY_DSN")
+SENTRY_LOG_LEVEL = env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO)
+
+sentry_logging = LoggingIntegration(
+    level=SENTRY_LOG_LEVEL,  # Capture info and above as breadcrumbs
+    event_level=logging.ERROR,  # Send errors as events
+)
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    integrations=[sentry_logging, DjangoIntegration(), CeleryIntegration()],
+
+    send_default_pii=True
+)
 
 # Your stuff...
 # ------------------------------------------------------------------------------
