d166ea44576843d11d0cc2065693bb6117c1ce77
wedwardbeck <wedwardbeck@gmail.com>
2020-02-26 21:43:07 -0600

    fixed token auth

--- config/settings/base.py
+++ config/settings/base.py
@@ -322,3 +322,14 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:8080",
     "http://localhost:3000",
 ]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
